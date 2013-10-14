d26a156755f4c2b007c53bc8f8c1b30434dcb4b0
Andreas Roth <andreas.roth@fastprotect.net>
2013-10-14 16:57:37 +0200

    saucy

--- changelog.py
+++ changelog.py
@@ -5,6 +5,11 @@
 package_changelog = [
 
     {
+        'version':'2.76', 'urgency':'low',
+        'items':['prepare for saucy'],
+        'author':'Andreas Roth <aroth@arsoft-online.com>', 'date':'Mon, 14 Oct 2013 16:56:16 +0200'
+    },
+    {
         'version':'2.75', 'urgency':'low',
         'items':['install digikam along with KDE desktop'],
         'author':'Andreas Roth <aroth@arsoft-online.com>', 'date':'Sat, 07 Sep 2013 09:12:41 +0200'
