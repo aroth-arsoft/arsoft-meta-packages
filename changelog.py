de12d7d8261e20f36e76eaa873f0b8123577a8aa
Andreas Roth <aroth@arsoft-online.com>
2014-04-14 19:37:48 +0200

    fixed gstreamer trouble

--- changelog.py
+++ changelog.py
@@ -5,6 +5,11 @@
 package_changelog = [
 
     {
+        'version':'2.112', 'urgency':'low',
+        'items':['fixed gstreamer trouble on trusty'],
+        'author':'Andreas Roth <aroth@arsoft-online.com>', 'date':'Mon, 14 Apr 2014 19:35:26 +0200'
+    },
+    {
         'version':'2.111', 'urgency':'low',
         'items':['prepare nvidia packages for next driver versions'],
         'author':'Andreas Roth <aroth@arsoft-online.com>', 'date':'Mon, 14 Apr 2014 09:39:08 +0200'
--- grp_desktop.py
+++ grp_desktop.py
@@ -6,6 +6,9 @@
 #                'gtk2-engines-murrine', 'gtk2-engines-pixbuf', 'gtk2-engines-qtcurve', 'gtk2-engines-aurora', 'gtk2-engines-blueheart', 'gtk2-engines-cleanice',
 #                'gtk2-engines-equinox', 'gtk2-engines-magicchicken', 'gtk2-engines-moblin', 'gtk2-engines-mythbuntu', 'gtk2-engines-nodoka', 'gtk2-engines-sapwood'
 #                'gtk2-engines-smooth', 'gtk2-engines-wonderland', 'gtk2-engines-xfce'
+# All gstreamer packages
+# 'gstreamer0.10-ffmpeg', 'gstreamer0.10-alsa', 'gstreamer0.10-gnonlin', 'gstreamer0.10-nice', 'gstreamer0.10-plugins-base-apps', 'gstreamer0.10-tools',
+# 'gstreamer0.10-plugins-good', 'gstreamer0.10-plugins-bad', 'gstreamer0.10-plugins-ugly',
 
 desktop = [
     {'name':'common', 
@@ -14,9 +17,6 @@ desktop = [
     'description':'', 
     'packages':['ttf-mscorefonts-installer', 
                 'policykit-desktop-privileges',
-                # All gstreamer packages
-                'gstreamer0.10-ffmpeg', 'gstreamer0.10-alsa', 'gstreamer0.10-gnonlin', 'gstreamer0.10-nice', 'gstreamer0.10-plugins-base-apps', 'gstreamer0.10-tools',
-                'gstreamer0.10-plugins-good', 'gstreamer0.10-plugins-bad', 'gstreamer0.10-plugins-ugly',
                 'unclutter', 
                 ],
     'conflicts':[
@@ -30,4 +30,3 @@ desktop = [
     'noconflicts':[]
     }
 ]
- 
