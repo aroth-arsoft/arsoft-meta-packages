dac72f191b1b6211db17f7b559c6b42847a881b0
Andreas Roth <aroth@arsoft-online.com>
2014-06-28 09:24:31 +0200

    use version number for distro instead of codename add kdevelop meta package

--- config.py
+++ config.py
@@ -2,10 +2,26 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
 
-all_distributions = ['hardy', 'intrepid', 'jaunty', 'lucid', 'karmic', 'maverick', 'natty', 'oneiric', 'precise', 'quantal', 'raring', 'saucy', 'trusty', 'utopic']
-default_distributions = ['precise', 'saucy', 'trusty', 'utopic']
+all_distributions = {
+    'hardy':'8.04',
+    'intrepid':'8.10',
+    'jaunty':'9.04',
+    'lucid':'9.10',
+    'karmic':'10.04',
+    'maverick':'10.10',
+    'natty':'11.04',
+    'oneiric':'11.10',
+    'precise':'12.04',
+    'quantal':'12.10',
+    'raring':'13.04',
+    'saucy':'13.10',
+    'trusty':'14.04',
+    'utopic':'14.10'
+}
+
+default_distributions = ['precise', 'trusty', 'utopic']
 source_package_name = 'arsoft-meta-packages'
 package_maintainer = 'Andreas Roth <aroth@arsoft-online.com>'
 package_homepage = 'https://github.com/aroth-arsoft/arsoft-meta-packages'
 package_debhelper = 5
-package_standardsversions = '3.9.4'
+package_standardsversions = '3.9.5'
