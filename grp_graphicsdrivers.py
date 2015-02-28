cdb3ebe4904d809eb5fe2ad2c0b992c4fc146b7f
Andreas Roth <aroth@arsoft-online.com>
2015-02-28 08:49:09 +0100

    fixed nvidia settings

--- grp_graphicsdrivers.py
+++ grp_graphicsdrivers.py
@@ -10,8 +10,9 @@ graphicsdrivers = [
     {'name':'nvidia', 'shortdesc':'Installs the NVIDIA graphics driver', 'description':'', 
         'packages':[ 
             ('nvidia-346', 'nvidia-343', 'nvidia-340', 'nvidia-337', 'nvidia-334', 'nvidia-331','nvidia-325','nvidia-319','nvidia-313','nvidia-304'),
-            ('nvidia-settings', 'nvidia-settings-331','nvidia-settings-325','nvidia-settings-319','nvidia-settings-313','nvidia-settings-304'),
+            ('nvidia-settings'),
             'v86d', 'vdpau-va-driver'],
+        'conflicts': ['nvidia-settings-331','nvidia-settings-325','nvidia-settings-319','nvidia-settings-313','nvidia-settings-304'],
         'noconflicts':['fglrx']
     },
     {'name':'nv', 'shortdesc':'Installs the free NVIDIA graphics driver', 'description':'', 'packages':['xserver-xorg-video-nv'] },
