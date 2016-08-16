4ec54cb7cb38c37a7aa8ec383ecc9e54b041e6ec
Andreas Roth <aroth@arsoft-online.com>
2016-08-16 19:18:47 +0200

    remove wily and build for yakkety

--- config.py
+++ config.py
@@ -20,9 +20,10 @@ all_distributions = {
     'vivid':'15.04',
     'wily':'15.10',
     'xenial':'16.04',
+    'yakkety':'16.10',
 }
 
-default_distributions = ['precise', 'trusty', 'wily', 'xenial']
+default_distributions = ['precise', 'trusty', 'xenial', 'yakkety']
 source_package_name = 'arsoft-meta-packages'
 package_maintainer = 'Andreas Roth <aroth@arsoft-online.com>'
 package_homepage = 'https://github.com/aroth-arsoft/arsoft-meta-packages'
